2502d7c8efab8ad08d4d464fa8858141dcae2a70
Onyx Mueller <onyx@onyxmueller.net>
2024-11-24 21:35:43 -0700

    feat: add groups add file test

--- test/pinata/resources/groups_test.rb
+++ test/pinata/resources/groups_test.rb
@@ -40,4 +40,14 @@ class GroupsResourceTest < Minitest::Test
     assert_equal "01934c6f-ec31-778c-96b7-111111111111", first_group.id
     assert_equal false, first_group.is_public
   end
+
+  def test_add_file
+    group_id = "01935c70-3b13-77dd-bd3b-000000000000"
+    file_id = "31d8fd7e-3a24-4e71-a1ad-d5fb8c035b38"
+    stub = stub_request("files/groups/#{group_id}/ids/#{file_id}", method: :put, response: stub_response(fixture: "groups/add_file"))
+    client = Pinata::Client.new(pinata_jwt: "fake", adapter: :test, stubs: stub)
+    empty_group = client.groups.add_file(group_id: group_id, file_id: file_id)
+
+    assert_equal Pinata::Group, empty_group.class
+  end
 end
